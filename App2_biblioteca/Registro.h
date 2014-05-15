960f82e22c30af94ea9af357b6b12b7c64c8c9b4
Victor Cotrim <cotrim149@gmail.com>
2014-05-15 16:13:10 -0300

    Adicionada tela de registro e classe de registro

---
+++ App2_biblioteca/Registro.h
@@ -0,0 +1,19 @@
+//
+//  Registro.h
+//  App2_biblioteca
+//
+//  Created by Victor de Lima on 15/05/14.
+//  Copyright (c) 2014 Victor de Lima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Registro : NSObject
+
+@property (nonatomic) NSString* titulo;
+@property (nonatomic) NSString* isbn;
+@property (nonatomic) NSString* autor;
+
+
+
+@end
---
+++ App2_biblioteca/Registro.m
@@ -0,0 +1,13 @@
+//
+//  Registro.m
+//  App2_biblioteca
+//
+//  Created by Victor de Lima on 15/05/14.
+//  Copyright (c) 2014 Victor de Lima. All rights reserved.
+//
+
+#import "Registro.h"
+
+@implementation Registro
+
+@end
